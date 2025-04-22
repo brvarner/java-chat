c47849385f23b27e31e00de58bebb6656a66db5e
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2025-04-21 20:02:38 -0500

    finished server and completing client

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,7 +1,36 @@
+package client;
+
 import java.io.*;
 import java.net.*;
 
 public class ChatClient {
+    private Socket socket = null;
+    private BufferedReader inputConsole = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+
+    public ChatClient(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected to the chat server");
+
+            inputConsole = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String line = "";
+            while(!line.equals("exit")){
+                line = inputConsole.readLine();
+                out.println(line);
+                System.out.println(in.readLine());
+            }
+
+            socket.close();
+            inputConsole.close();
+            out.close();
+        }
+    }
+
     public static void main(String[] args) {
         Socket socket = new Socket("localhost", 5000);
         System.out.println("Connected to server.");
