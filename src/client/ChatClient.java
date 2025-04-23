d9a4cb4c3d0e82c5da3273ef8cb04aee924503b4
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2025-04-22 20:04:55 -0500

    Created chat client GUI

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -2,42 +2,36 @@ package client;
 
 import java.io.*;
 import java.net.*;
+import java.util.function.Consumer;
 
 public class ChatClient {
-    private Socket socket = null;
-    private BufferedReader inputConsole = null;
-    private PrintWriter out = null;
-    private BufferedReader in = null;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private Consumer<String> onMessageReceived;
 
-    public ChatClient(String address, int port) {
-        try {
-            socket = new Socket(address, port);
-            System.out.println("Connected to the chat server");
+    public ChatClient(String serverAddress, int serverPort, Consumer<String> onMessageReceived) throws IOException {
+        this.socket = new Socket(serverAddress, serverPort);
+        System.out.println("Connected to the chat server");
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+    }
 
-            inputConsole = new BufferedReader(new InputStreamReader(System.in));
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public void sendMessage(String msg) {
+        out.println(msg);
+    }
 
-            String line = "";
-            while (!line.equals("exit")) {
-                line = inputConsole.readLine();
-                out.println(line);
-                System.out.println(in.readLine());
+    public void startClient() {
+        new Thread(() -> {
+            try {
+                String line;
+                while ((line = in.readLine()) != null) {
+                    onMessageReceived.accept(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            socket.close();
-            inputConsole.close();
-            out.close();
-        } catch (UnknownHostException u) {
-            String msg = String.format("Host unknown: %s", u.getMessage());
-            System.out.println(msg);
-        } catch (IOException i) {
-            String msg = String.format("Unexpected exception: %s", i.getMessage());
-            System.out.println(msg);
-        }
+        }).start();
     }
 
-    public static void main(String[] args) {
-        ChatClient client = new ChatClient("127.0.0.1", 5000);
-    }
 }
